12e6389c655bb156e4115e4291e55e1d9404a972
Roger2608 <62685372+Roger2608@users.noreply.github.com>
2024-03-01 00:44:01 -0500

    up scaffolding back

---
+++ planning-activity-back/src/main/java/com/planning/planningactivity/PlanningActivityBackApplication.java
@@ -0,0 +1,13 @@
+package com.planning.planningactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningActivityBackApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningActivityBackApplication.class, args);
+	}
+	
+}
---
+++ planning-activity-back/src/test/java/com/planning/planningactivity/PlanningActivityBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planning.planningactivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningActivityBackApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
